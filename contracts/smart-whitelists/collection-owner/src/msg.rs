6662e5efcf3a9b7372f411da5f0e26ed7246170c
Shane Vitarana <shanev@gmail.com>
2023-06-02 19:57:43 -0400

    Add collection owner smart whitelist

---
+++ contracts/smart-whitelists/collection-owner/src/msg.rs
@@ -0,0 +1,23 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub collection: String,
+}
+
+#[cw_serde]
+pub enum ExecuteMsg {}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    #[returns(ConfigResponse)]
+    Config {},
+    #[returns(bool)]
+    IncludesAddress { address: String },
+}
+
+#[cw_serde]
+pub struct ConfigResponse {
+    pub collection: String,
+}
