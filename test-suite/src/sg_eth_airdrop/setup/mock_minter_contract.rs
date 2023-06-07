d86a23f231584455b3dd3b38a875f48bc95977a6
John Y <j@publicawesome.com>
2023-06-07 11:01:04 -0400

    use mock_query_config in sg-eth-airdrop tests

--- test-suite/src/sg_eth_airdrop/setup/mock_minter_contract.rs
+++ test-suite/src/sg_eth_airdrop/setup/mock_minter_contract.rs
@@ -1,27 +1,13 @@
-use cosmwasm_std::entry_point;
-use cosmwasm_std::{
-    to_binary, Binary, Coin, Deps, DepsMut, Env, MessageInfo, StdResult, Timestamp,
-};
+use cosmwasm_std::{entry_point, Deps};
+use cosmwasm_std::{to_binary, Binary, DepsMut, Env, MessageInfo, StdResult};
 use cw_multi_test::{Contract, ContractWrapper};
+use sg4::MinterConfigResponse;
 use sg_eth_airdrop::error::ContractError;
 use sg_std::{Response, StargazeMsgWrapper};
 use vending_factory::msg::VendingMinterCreateMsg;
 use vending_minter::msg::{ExecuteMsg, QueryMsg};
-
-use cosmwasm_schema::cw_serde;
-#[cw_serde]
-pub struct ConfigResponse {
-    pub admin: String,
-    pub base_token_uri: String,
-    pub num_tokens: u32,
-    pub per_address_limit: u32,
-    pub sg721_address: String,
-    pub sg721_code_id: u64,
-    pub start_time: Timestamp,
-    pub mint_price: Coin,
-    pub whitelist: Option<String>,
-    pub factory: String,
-}
+use vending_minter::state::ConfigExtension;
+use vending_minter::tests::mock_query_config;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -51,19 +37,8 @@ pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     }
 }
 
-fn query_config() -> ConfigResponse {
-    ConfigResponse {
-        admin: "some_admin".to_string(),
-        whitelist: Some("contract2".to_string()),
-        base_token_uri: "some_uri".to_string(),
-        num_tokens: 5,
-        per_address_limit: 5,
-        sg721_address: "some_sg721_address".to_string(),
-        sg721_code_id: 4,
-        start_time: Timestamp::from_seconds(30),
-        mint_price: Coin::new(1000, "ustars"),
-        factory: "some_factory".to_string(),
-    }
+fn query_config() -> MinterConfigResponse<ConfigExtension> {
+    mock_query_config()
 }
 
 pub fn mock_minter() -> Box<dyn Contract<StargazeMsgWrapper>> {
